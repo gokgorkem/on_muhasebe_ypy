cebc3e35466b5e0c8bdbd7cb2f839555a65415ff
Engin Çağlar <36919345+engncaglar13@users.noreply.github.com>
2021-06-24 16:27:04 +0300

    Update yeni fatura alislar
    Updated yeni fatura alislar

--- lib/features/yeni_fatura_alislar/presentation/widgets/yeni_fatura_alislar_body.dart
+++ lib/features/yeni_fatura_alislar/presentation/widgets/yeni_fatura_alislar_body.dart
@@ -19,6 +19,156 @@ class _YeniFaturaAlislarBodyState extends State<YeniFaturaAlislarBody> {
   @override
   Widget build(BuildContext context) {
     return Container(
+      child: Form(
+        key: _formKey,
+        child: ListView(
+          children: <Widget>[
+            Row(
+              children: [
+                Expanded(
+                  flex: 1,
+                  //TODO changed to serch field
+                  child: TextFormField(
+                    decoration: InputDecoration(
+                      hintText: "Tedarikçi ",
+                      labelText: "Tedarikçi  Seçiniz",
+                      border: OutlineInputBorder(),
+                    ),
+                  ),
+                ),
+                SizedBox(
+                  width: 10,
+                ),
+                Expanded(
+                  child: Container(
+                    child: DropdownButton(
+                      hint: Text("Para Birimi Seçiniz"),
+                      items: [
+                        DropdownMenuItem(
+                          child: Text("Türk Lirası"),
+                          value: 1,
+                          onTap: () => null,
+                        ),
+                        DropdownMenuItem(
+                          child: Text("Dolar"),
+                          value: 2,
+                          onTap: () => null,
+                        ),
+                      ],
+                      onChanged: (value) => null,
+                    ),
+                  ),
+                ),
+              ],
+            ),
+            SizedBox(
+              height: 10,
+            ),
+            Row(
+              children: [
+                Expanded(
+                  flex: 1,
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: [
+                      Text("Fatura Tarihi : "),
+                      TextButton(
+                        onPressed: () {
+                          DatePicker.showDatePicker(
+                            context,
+                            showTitleActions: true,
+                            minTime: DateTime(DateTime.now().year - 5),
+                            maxTime: DateTime(DateTime.now().year + 5),
+                            onConfirm: (date) {
+                              setState(() {
+                                _date = date;
+                              });
+                            },
+                            currentTime: DateTime.now(),
+                            locale: LocaleType.tr,
+                          );
+                        },
+                        child: Text(formatDate.format(_date)),
+                      ),
+                    ],
+                  ),
+                ),
+                SizedBox(
+                  width: 10,
+                ),
+                Expanded(
+                  flex: 1,
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: [
+                      Text("Vade Tarihi : "),
+                      TextButton(
+                        onPressed: () {
+                          DatePicker.showDatePicker(
+                            context,
+                            showTitleActions: true,
+                            minTime: DateTime(DateTime.now().year - 5),
+                            maxTime: DateTime(DateTime.now().year + 5),
+                            onConfirm: (date) {
+                              setState(() {
+                                _date = date;
+                              });
+                            },
+                            currentTime: DateTime.now(),
+                            locale: LocaleType.tr,
+                          );
+                        },
+                        child: Text(formatDate.format(_date)),
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+            SizedBox(
+              height: 10,
+            ),
+            Row(
+              children: [
+                Expanded(
+                  child: Container(
+                    decoration: BoxDecoration(),
+                    child: DropdownButton(
+                      hint: Text("Fatura Kategoıri"),
+                      items: [
+                        DropdownMenuItem(
+                          child: Text("Alış"),
+                          value: 1,
+                          onTap: () => null,
+                        ),
+                        DropdownMenuItem(
+                          child: Text("Diğer"),
+                          value: 2,
+                          onTap: () => null,
+                        ),
+                      ],
+                      onChanged: (value) => null,
+                    ),
+                  ),
+                ),
+                SizedBox(
+                  width: 10,
+                ),
+                Expanded(
+                  child: TextFormField(
+                    initialValue: "sipariş 1",
+                    decoration: InputDecoration(
+                      hintText: "Sipariş No",
+                      labelText: "Sipariş No Giriniz",
+                      border: OutlineInputBorder(),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
