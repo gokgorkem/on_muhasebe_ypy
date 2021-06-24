da22438d76d26f21e94cedbbc367883818e0157c
Engin Çağlar <36919345+engncaglar13@users.noreply.github.com>
2021-06-24 22:53:00 +0300

    Update yeni tahsilat satislar body add datetimepicker
    Updated yeni tahsilat satislar body add datetimepicker for tahsilat tarihi

--- lib/features/yeni_tahsilat_satislar/presentation/widgets/yeni_tahsilat_satislar_body.dart
+++ lib/features/yeni_tahsilat_satislar/presentation/widgets/yeni_tahsilat_satislar_body.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_datetime_picker/flutter_datetime_picker.dart';
 import 'package:intl/intl.dart';
 import 'package:on_muhasebe/core/utils/model_base.dart';
 
@@ -20,7 +21,41 @@ class _YeniTahsilatSatislarBodyState extends State<YeniTahsilatSatislarBody> {
   Widget build(BuildContext context) {
     return Form(
       key: _formKey,
-      child: ListView(),
+      child: ListView(
+        children: [
+          Row(
+            children: [
+              Expanded(
+                flex: 1,
+                child: Row(
+                  children: [
+                    Text("Tahsilat Tarihi :"),
+                    TextButton(
+                      onPressed: () {
+                        DatePicker.showDatePicker(
+                          context,
+                          showTitleActions: true,
+                          minTime: DateTime(DateTime.now().year - 5),
+                          maxTime: DateTime(DateTime.now().year + 5),
+                          onConfirm: (date) {
+                            setState(() {
+                              _date = date;
+                            });
+                          },
+                          currentTime: DateTime.now(),
+                          locale: LocaleType.tr,
+                        );
+                      },
+                      child: Text(formatDate.format(_date)),
+                    ),
+                  ],
+                ),
+              ),
+              SizedBox(width: 10),
+            ],
+          )
+        ],
+      ),
     );
   }
 }
