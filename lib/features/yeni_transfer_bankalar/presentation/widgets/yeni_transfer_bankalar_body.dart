2b77a9e09fe4a25b73bd003108df5b1acf0ec9bc
Engin Çağlar <36919345+engncaglar13@users.noreply.github.com>
2021-06-24 16:46:33 +0300

    Update Yeni transfer bankalar widgets
    Updated Yeni transfer bankalar widgets

--- lib/features/yeni_transfer_bankalar/presentation/widgets/yeni_transfer_bankalar_body.dart
+++ lib/features/yeni_transfer_bankalar/presentation/widgets/yeni_transfer_bankalar_body.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_datetime_picker/flutter_datetime_picker.dart';
 import 'package:intl/intl.dart';
 import 'package:on_muhasebe/core/utils/model_base.dart';
 
@@ -23,9 +24,6 @@ class _YeniTransferBankalarBodyState extends State<YeniTransferBankalarBody> {
         key: _formKey,
         child: ListView(
           children: <Widget>[
-            SizedBox(
-              height: 10,
-            ),
             Row(
               children: [
                 Expanded(
@@ -54,7 +52,7 @@ class _YeniTransferBankalarBodyState extends State<YeniTransferBankalarBody> {
                 Expanded(
                   child: Container(
                     child: DropdownButton(
-                      hint: Text("Gönderilen Hesap"),
+                      hint: Text("Alan Hesap"),
                       items: [
                         DropdownMenuItem(
                           child: Text("Kasa"),
@@ -75,6 +73,95 @@ class _YeniTransferBankalarBodyState extends State<YeniTransferBankalarBody> {
             ),
             SizedBox(
               height: 10,
+            ),
+            Row(
+              children: [
+                Expanded(
+                  flex: 1,
+                  child: TextFormField(
+                    decoration: InputDecoration(
+                      hintText: "Tutar ",
+                      labelText: "Tutar Giriniz",
+                      border: OutlineInputBorder(),
+                    ),
+                  ),
+                ),
+                SizedBox(
+                  width: 10,
+                ),
+                Expanded(
+                  flex: 1,
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: [
+                      Text("Tarihi : "),
+                      TextButton(
+                        onPressed: () {
+                          DatePicker.showDatePicker(
+                            context,
+                            showTitleActions: true,
+                            minTime: DateTime(DateTime.now().year - 5),
+                            maxTime: DateTime(DateTime.now().year + 5),
+                            onConfirm: (date) {
+                              setState(() {
+                                _date = date;
+                              });
+                            },
+                            currentTime: DateTime.now(),
+                            locale: LocaleType.tr,
+                          );
+                        },
+                        child: Text(formatDate.format(_date)),
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+            SizedBox(
+              height: 10,
+            ),
+            Row(
+              children: [
+                Expanded(
+                  flex: 1,
+                  child: TextFormField(
+                    decoration: InputDecoration(
+                      hintText: "Açıklama ",
+                      labelText: "Açıklama Giriniz",
+                      border: OutlineInputBorder(),
+                    ),
+                  ),
+                ),
+                SizedBox(
+                  width: 10,
+                ),
+                Expanded(
+                  child: Container(
+                    child: DropdownButton(
+                      hint: Text("Ödeme Yöntemi"),
+                      items: [
+                        DropdownMenuItem(
+                          child: Text("Nakit"),
+                          value: 1,
+                          onTap: () => null,
+                        ),
+                        DropdownMenuItem(
+                          child: Text("Havale"),
+                          value: 2,
+                          onTap: () => null,
+                        ),
+                        DropdownMenuItem(
+                          child: Text("EFT"),
+                          value: 3,
+                          onTap: () => null,
+                        ),
+                      ],
+                      onChanged: (value) => null,
+                    ),
+                  ),
+                ),
+              ],
             ),
           ],
         ),
