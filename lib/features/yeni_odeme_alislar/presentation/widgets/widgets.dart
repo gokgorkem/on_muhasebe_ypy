24a97c3587d75f1c88bad9ea73a1e1c8acb1ee45
Görkem Gök <gorkemgok475@gmail.com>
2021-06-24 15:06:01 +0300

    CASE 34 Created blank yeni odeme alislar widgets
    Created yeni odeme alislar widgets, to be updated by the Engin

---
+++ lib/features/yeni_odeme_alislar/presentation/widgets/widgets.dart
@@ -0,0 +1,3 @@
+export 'loading_widget.dart';
+export 'messagge_display.dart';
+export 'yeni_odeme_alislar_body.dart';
---
+++ lib/features/yeni_odeme_alislar/presentation/widgets/yeni_odeme_alislar_body.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:on_muhasebe/core/utils/model_base.dart';
+
+class YeniOdemeAlislarBody extends StatefulWidget {
+  const YeniOdemeAlislarBody({Key? key}) : super(key: key);
+
+  @override
+  _YeniOdemeAlislarBodyState createState() => _YeniOdemeAlislarBodyState();
+}
+
+class _YeniOdemeAlislarBodyState extends State<YeniOdemeAlislarBody> {
+  final _formKey = GlobalKey<FormState>(debugLabel: "asd");
+  late EntitiesModel entitiesModel;
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+    );
+  }
+}
