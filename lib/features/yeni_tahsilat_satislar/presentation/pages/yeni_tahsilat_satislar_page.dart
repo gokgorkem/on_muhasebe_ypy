b3ae379628775e7bc20e837f3e50f784ddd14ef4
Engin Çağlar <36919345+engncaglar13@users.noreply.github.com>
2021-06-24 22:47:18 +0300

    Update yeni tahsilat satislar page
    Updated yeni tahsilat satislar page

--- lib/features/yeni_tahsilat_satislar/presentation/pages/yeni_tahsilat_satislar_page.dart
+++ lib/features/yeni_tahsilat_satislar/presentation/pages/yeni_tahsilat_satislar_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:on_muhasebe/core/widgets/page_root.dart';
+import 'package:on_muhasebe/features/yeni_tahsilat_satislar/presentation/widgets/widgets.dart';
 
 class YeniTahsilatSatislarPage extends StatefulWidget {
   const YeniTahsilatSatislarPage({Key? key}) : super(key: key);
@@ -13,7 +14,7 @@ class _YeniTahsilatSatislarPageState extends State<YeniTahsilatSatislarPage> {
   @override
   Widget build(BuildContext context) {
     return PageRoot.withBody(
-      body: Container(),
+      body: YeniTahsilatSatislarBody(),
     );
   }
 }
