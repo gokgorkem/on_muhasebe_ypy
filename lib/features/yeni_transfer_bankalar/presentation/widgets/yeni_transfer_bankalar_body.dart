2af711982ced7120427fd73132c05caa820787a4
Engin Çağlar <36919345+engncaglar13@users.noreply.github.com>
2021-06-24 16:40:23 +0300

    Update Yeni transfer bankalar widgets
    Updated Yeni transfer bankalar widgets

--- lib/features/yeni_transfer_bankalar/presentation/widgets/yeni_transfer_bankalar_body.dart
+++ lib/features/yeni_transfer_bankalar/presentation/widgets/yeni_transfer_bankalar_body.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_datetime_picker/flutter_datetime_picker.dart';
 import 'package:intl/intl.dart';
 import 'package:on_muhasebe/core/utils/model_base.dart';
 
@@ -8,7 +7,8 @@ class YeniTransferBankalarBody extends StatefulWidget {
   const YeniTransferBankalarBody({Key? key}) : super(key: key);
 
   @override
-  _YeniTransferBankalarBodyState createState() => _YeniTransferBankalarBodyState();
+  _YeniTransferBankalarBodyState createState() =>
+      _YeniTransferBankalarBodyState();
 }
 
 class _YeniTransferBankalarBodyState extends State<YeniTransferBankalarBody> {
@@ -19,6 +19,66 @@ class _YeniTransferBankalarBodyState extends State<YeniTransferBankalarBody> {
   @override
   Widget build(BuildContext context) {
     return Container(
+      child: Form(
+        key: _formKey,
+        child: ListView(
+          children: <Widget>[
+            SizedBox(
+              height: 10,
+            ),
+            Row(
+              children: [
+                Expanded(
+                  child: Container(
+                    child: DropdownButton(
+                      hint: Text("Gönderilen Hesap"),
+                      items: [
+                        DropdownMenuItem(
+                          child: Text("Kasa"),
+                          value: 1,
+                          onTap: () => null,
+                        ),
+                        DropdownMenuItem(
+                          child: Text("Banka"),
+                          value: 2,
+                          onTap: () => null,
+                        ),
+                      ],
+                      onChanged: (value) => null,
+                    ),
+                  ),
+                ),
+                SizedBox(
+                  width: 10,
+                ),
+                Expanded(
+                  child: Container(
+                    child: DropdownButton(
+                      hint: Text("Gönderilen Hesap"),
+                      items: [
+                        DropdownMenuItem(
+                          child: Text("Kasa"),
+                          value: 1,
+                          onTap: () => null,
+                        ),
+                        DropdownMenuItem(
+                          child: Text("Banka"),
+                          value: 2,
+                          onTap: () => null,
+                        ),
+                      ],
+                      onChanged: (value) => null,
+                    ),
+                  ),
+                ),
+              ],
+            ),
+            SizedBox(
+              height: 10,
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
