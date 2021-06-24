925d73515dcfdbf14d99c971fac294938f8d58cc
Görkem Gök <gorkemgok475@gmail.com>
2021-06-24 15:50:31 +0300

    Created blank yeni tedarikci alislar widgets

---
+++ lib/features/yeni_tedarikci_alislar/presentation/widgets/widgets.dart
@@ -0,0 +1,3 @@
+export 'loading_widget.dart';
+export 'messagge_display.dart';
+export 'yeni_tedarikci_alislar_body.dart';
---
+++ lib/features/yeni_tedarikci_alislar/presentation/widgets/yeni_tedarikci_alislar_body.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:on_muhasebe/core/utils/model_base.dart';
+
+class YeniTedarikciAlislarBody extends StatefulWidget {
+  const YeniTedarikciAlislarBody({Key? key}) : super(key: key);
+
+  @override
+  _YeniTedarikciAlislarBodyState createState() => _YeniTedarikciAlislarBodyState();
+}
+
+class _YeniTedarikciAlislarBodyState extends State<YeniTedarikciAlislarBody> {
+  final _formKey = GlobalKey<FormState>(debugLabel: "asd");
+  late EntitiesModel entitiesModel;
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+    );
+  }
+}
