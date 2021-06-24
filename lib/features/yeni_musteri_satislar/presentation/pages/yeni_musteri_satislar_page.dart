1cca7d46bffe60e7e848db02e1f61ca17c57020a
Engin Çağlar <36919345+engncaglar13@users.noreply.github.com>
2021-06-24 21:36:22 +0300

    Case 29 Create yeni musteri satislar page
    Created yeni musteri satislar page

---
+++ lib/features/yeni_musteri_satislar/presentation/pages/yeni_musteri_satislar_page.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'package:on_muhasebe/core/widgets/page_root.dart';
+
+class YeniMusteriSatislarPage extends StatefulWidget {
+  const YeniMusteriSatislarPage({Key? key}) : super(key: key);
+
+  @override
+  _YeniMusteriSatislarPageState createState() =>
+      _YeniMusteriSatislarPageState();
+}
+
+class _YeniMusteriSatislarPageState extends State<YeniMusteriSatislarPage> {
+  @override
+  Widget build(BuildContext context) {
+    return PageRoot.withBody(
+      body: Container(),
+    );
+  }
+}
