8c8fb53dfec870138edf9d857237379fed6ab8c3
Engin Çağlar <36919345+engncaglar13@users.noreply.github.com>
2021-06-24 22:40:35 +0300

    Case 27 Create yeni tahsilat satislar page
    Created yeni tahsilat satislar page

---
+++ lib/features/yeni_tahsilat_satislar/presentation/pages/yeni_tahsilat_satislar_page.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'package:on_muhasebe/core/widgets/page_root.dart';
+
+class YeniTahsilatSatislarPage extends StatefulWidget {
+  const YeniTahsilatSatislarPage({Key? key}) : super(key: key);
+
+  @override
+  _YeniTahsilatSatislarPageState createState() =>
+      _YeniTahsilatSatislarPageState();
+}
+
+class _YeniTahsilatSatislarPageState extends State<YeniTahsilatSatislarPage> {
+  @override
+  Widget build(BuildContext context) {
+    return PageRoot.withBody(
+      body: Container(),
+    );
+  }
+}
