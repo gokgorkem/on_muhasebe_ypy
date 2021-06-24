70c46d76244c4d3c7a2b20756e454ca4c133005c
Engin Çağlar <36919345+engncaglar13@users.noreply.github.com>
2021-06-24 15:57:49 +0300

    Case 36 Update yeni tedarikci alislar widgets
    Updated yeni tedarikci alislar widgets

--- lib/features/yeni_tedarikci_alislar/presentation/widgets/yeni_tedarikci_alislar_body.dart
+++ lib/features/yeni_tedarikci_alislar/presentation/widgets/yeni_tedarikci_alislar_body.dart
@@ -6,7 +6,8 @@ class YeniTedarikciAlislarBody extends StatefulWidget {
   const YeniTedarikciAlislarBody({Key? key}) : super(key: key);
 
   @override
-  _YeniTedarikciAlislarBodyState createState() => _YeniTedarikciAlislarBodyState();
+  _YeniTedarikciAlislarBodyState createState() =>
+      _YeniTedarikciAlislarBodyState();
 }
 
 class _YeniTedarikciAlislarBodyState extends State<YeniTedarikciAlislarBody> {
@@ -15,6 +16,108 @@ class _YeniTedarikciAlislarBodyState extends State<YeniTedarikciAlislarBody> {
   @override
   Widget build(BuildContext context) {
     return Container(
+      child: Form(
+        key: _formKey,
+        child: ListView(
+          children: <Widget>[
+            Row(
+              children: [
+                Expanded(
+                  flex: 1,
+                  child: TextFormField(
+                    decoration: InputDecoration(
+                      hintText: "Tedarikçi İsim",
+                      labelText: "Tedarikçi İsimi Giriniz",
+                      border: OutlineInputBorder(),
+                    ),
+                  ),
+                ),
+                SizedBox(
+                  width: 10,
+                ),
+                Expanded(
+                  flex: 1,
+                  child: TextFormField(
+                    decoration: InputDecoration(
+                      hintText: "Tedarikçi Eposta",
+                      labelText: "Tedarikçi Eposta Giriniz",
+                      border: OutlineInputBorder(),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+            SizedBox(
+              height: 10,
+            ),
+            Row(
+              children: [
+                Expanded(
+                  flex: 1,
+                  child: TextFormField(
+                    decoration: InputDecoration(
+                      hintText: "Tedarikçi Eposta",
+                      labelText: "Tedarikçi Eposta Giriniz",
+                      border: OutlineInputBorder(),
+                    ),
+                  ),
+                ),
+                SizedBox(
+                  width: 10,
+                ),
+                Expanded(
+                  child: Container(
+                    child: DropdownButton(
+                      hint: Text("Para Birimi Seçiniz"),
+                      items: [
+                        DropdownMenuItem(
+                          child: Text("Türk Lirası"),
+                          value: 1,
+                          onTap: () => null,
+                        ),
+                        DropdownMenuItem(
+                          child: Text("Dolar"),
+                          value: 2,
+                          onTap: () => null,
+                        ),
+                      ],
+                      onChanged: (value) => null,
+                    ),
+                  ),
+                ),
+              ],
+            ),
+            SizedBox(
+              height: 10,
+            ),
+            Row(
+              children: [
+                Expanded(
+                  child: TextFormField(
+                    decoration: InputDecoration(
+                      hintText: "Telefon Numarası",
+                      labelText: "Telefon Numarası Giriniz",
+                      border: OutlineInputBorder(),
+                    ),
+                  ),
+                ),
+                SizedBox(
+                  width: 10,
+                ),
+                Expanded(
+                  child: TextFormField(
+                    decoration: InputDecoration(
+                      hintText: "Tedarikçi Adres",
+                      labelText: "Tedarikçi Adres Giriniz",
+                      border: OutlineInputBorder(),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
