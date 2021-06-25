756cbb86014c445a4e7ca4d6e05c6e9e6a369aa9
Engin Çağlar <86078382+gokgorkem@users.noreply.github.com>
2021-06-25 11:52:10 +0300

    Update yeni odeme alislar body kaydet and iptal button
     Updated yeni odeme alislar body kaydet and iptal button for navigation

--- lib/features/yeni_odeme_alislar/presentation/widgets/yeni_odeme_alislar_body.dart
+++ lib/features/yeni_odeme_alislar/presentation/widgets/yeni_odeme_alislar_body.dart
@@ -2,6 +2,7 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_datetime_picker/flutter_datetime_picker.dart';
 import 'package:intl/intl.dart';
+import 'package:on_muhasebe/core/utils/constants.dart';
 import 'package:on_muhasebe/core/utils/model_base.dart';
 
 class YeniOdemeAlislarBody extends StatefulWidget {
@@ -199,7 +200,10 @@ class _YeniOdemeAlislarBodyState extends State<YeniOdemeAlislarBody> {
     );
   }
 
-  _onPressedIptal() {}
+  _onPressedIptal() {
+    Navigator.of(context).pushNamed(ALISLAR_ODEMELER_PAGE_ROUTE);
+  }
 
-  _onPressedKaydet() {}
+  _onPressedKaydet() {
+    Navigator.of(context).pushNamed(ALISLAR_ODEMELER_PAGE_ROUTE);}
 }
