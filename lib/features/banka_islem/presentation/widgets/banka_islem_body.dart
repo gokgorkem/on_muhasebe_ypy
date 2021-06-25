a0400fa1d83180d9e7e5cf36df0ec57852a9ecd1
Engin Çağlar <86078382+gokgorkem@users.noreply.github.com>
2021-06-25 15:15:34 +0300

    Update banka_islem_body.dart
    Update banka islem page body add gelir ekle and gider ekle buttons

--- lib/features/banka_islem/presentation/widgets/banka_islem_body.dart
+++ lib/features/banka_islem/presentation/widgets/banka_islem_body.dart
@@ -15,39 +15,61 @@ class BankaIslemBody extends StatefulWidget {
 class _BankaIslemBodyState extends State<BankaIslemBody> {
   @override
   Widget build(BuildContext context) {
-    return ListView.separated(
-      separatorBuilder: (context, index) => Divider(),
-      itemCount: (widget.body == null || widget.headers == null)
-          ? 1
-          : widget.body.length + 1,
-      itemBuilder: (BuildContext context, int index) {
-        if (index == 0) {
-          return Container(
-            padding: EdgeInsets.all(10),
-            decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(10),
-                color: CustomColor.logoBlue),
-            child: new Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: _buildHeaders(),
+    return Column(
+      children: [
+        Expanded(flex: 1,child: Container(child: Row(
+          children: [
+            Expanded(flex: 1,child: Container(),),
+            Expanded(flex: 1,
+              child: Row(
+                children: [
+                  Expanded(flex: 1, child: TextButton(onPressed: () => null,child: Text("Gelir Ekle"),)),
+                  Expanded(flex: 1, child: TextButton(onPressed: () => null,child: Text("Gider Ekle"),)),
+                ],
+              ),
             ),
-          );
-        }
-        index -= 1;
+          ],
+        ),)),
+        Expanded(flex: 8,child: _buildListViewBankaIslem()),
+        Expanded(flex: 1,child: Container())
+      ],
+    );
+  }
+
+  ListView _buildListViewBankaIslem() {
+    return ListView.separated(
+    separatorBuilder: (context, index) => Divider(),
+    itemCount: (widget.body == null || widget.headers == null)
+        ? 1
+        : widget.body.length + 1,
+    itemBuilder: (BuildContext context, int index) {
+      if (index == 0) {
         return Container(
           padding: EdgeInsets.all(10),
           decoration: BoxDecoration(
               borderRadius: BorderRadius.circular(10),
-              color: index % 2 == 0
-                  ? Colors.blue.shade700
-                  : CustomColor.statusBarColor),
-          child: Row(
+              color: CustomColor.logoBlue),
+          child: new Row(
             mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: _buildBody(index),
+            children: _buildHeaders(),
           ),
         );
-      },
-    );
+      }
+      index -= 1;
+      return Container(
+        padding: EdgeInsets.all(10),
+        decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(10),
+            color: index % 2 == 0
+                ? Colors.blue.shade700
+                : CustomColor.statusBarColor),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: _buildBody(index),
+        ),
+      );
+    },
+  );
   }
 
   List<Widget> _buildHeaders() {
