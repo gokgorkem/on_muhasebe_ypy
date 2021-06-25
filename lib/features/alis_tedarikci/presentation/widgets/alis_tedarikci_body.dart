5c37d8696f7b3b7c051050f3b520feb9b0b64747
Engin Çağlar <86078382+gokgorkem@users.noreply.github.com>
2021-06-25 15:07:30 +0300

    Update alis tedarikci page body
    Update alis tedarikci page body add yeni tedarikci button

--- lib/features/alis_tedarikci/presentation/widgets/alis_tedarikci_body.dart
+++ lib/features/alis_tedarikci/presentation/widgets/alis_tedarikci_body.dart
@@ -15,39 +15,49 @@ class AlislarTedarikciBody extends StatefulWidget {
 class _AlislarTedarikciBodyState extends State<AlislarTedarikciBody> {
   @override
   Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Expanded(flex: 1,child: Container(child: TextButton(onPressed: () => null,child: Text("Yeni Tedarikci"),),)),
+        Expanded(flex: 8,child: _buildListViewAlisTedarikci()),
+        Expanded(flex: 1,child: Container())
+      ],
+    );
+  }
+
+  ListView _buildListViewAlisTedarikci() {
     return ListView.separated(
-      separatorBuilder: (context, index) => Divider(),
-      itemCount: (widget.body == null || widget.headers == null)
-          ? 1
-          : widget.body.length + 1,
-      itemBuilder: (BuildContext context, int index) {
-        if (index == 0) {
-          return Container(
-            padding: EdgeInsets.all(10),
-            decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(10),
-                color: CustomColor.logoBlue),
-            child: new Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: _buildHeaders(),
-            ),
-          );
-        }
-        index -= 1;
+    separatorBuilder: (context, index) => Divider(),
+    itemCount: (widget.body == null || widget.headers == null)
+        ? 1
+        : widget.body.length + 1,
+    itemBuilder: (BuildContext context, int index) {
+      if (index == 0) {
         return Container(
           padding: EdgeInsets.all(10),
           decoration: BoxDecoration(
               borderRadius: BorderRadius.circular(10),
-              color: index % 2 == 0
-                  ? Colors.blue.shade700
-                  : CustomColor.statusBarColor),
-          child: Row(
+              color: CustomColor.logoBlue),
+          child: new Row(
             mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: _buildBody(index),
+            children: _buildHeaders(),
           ),
         );
-      },
-    );
+      }
+      index -= 1;
+      return Container(
+        padding: EdgeInsets.all(10),
+        decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(10),
+            color: index % 2 == 0
+                ? Colors.blue.shade700
+                : CustomColor.statusBarColor),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: _buildBody(index),
+        ),
+      );
+    },
+  );
   }
 
   List<Widget> _buildHeaders() {
