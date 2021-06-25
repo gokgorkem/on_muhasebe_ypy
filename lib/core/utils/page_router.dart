62016b98e62cccd3ea1f8ac245cb54d8ba02170f
Engin Çağlar <36919345+engncaglar13@users.noreply.github.com>
2021-06-25 17:16:19 +0300

    Update page_router.dart
    Page redirects updated

--- lib/core/utils/page_router.dart
+++ lib/core/utils/page_router.dart
@@ -13,7 +13,15 @@ import 'package:on_muhasebe/features/satis_fatura/presentation/pages/satis_fatur
 import 'package:on_muhasebe/features/satis_musteri/presentation/pages/satis_musteri_page.dart';
 import 'package:on_muhasebe/features/satis_tahsilat/presentation/pages/satis_tahsilat_page.dart';
 import 'package:on_muhasebe/features/urun_hizmet/presentation/pages/urun_hizmet_page.dart';
+import 'package:on_muhasebe/features/yeni_fatura_alislar/presentation/pages/yeni_fatura_alislar_page.dart';
+import 'package:on_muhasebe/features/yeni_fatura_satislar/presentation/pages/yeni_fatura_satislar_page.dart';
+import 'package:on_muhasebe/features/yeni_hesap_bankalar/presentation/pages/yeni_hesap_bankalar_page.dart';
+import 'package:on_muhasebe/features/yeni_musteri_satislar/presentation/pages/yeni_musteri_satislar_page.dart';
+import 'package:on_muhasebe/features/yeni_odeme_alislar/presentation/pages/yeni_odeme_alislar_page.dart';
 import 'package:on_muhasebe/features/yeni_tahsilat_satislar/presentation/pages/yeni_tahsilat_satislar_page.dart';
+import 'package:on_muhasebe/features/yeni_tedarikci_alislar/presentation/pages/yeni_tedarikci_alislar_page.dart';
+import 'package:on_muhasebe/features/yeni_transfer_bankalar/presentation/pages/yeni_transfer_bankalar_page.dart';
+import 'package:on_muhasebe/features/yeni_urun_hizmet/presentation/pages/yeni_urun_hizmet_page.dart';
 
 class PageRouter {
   static Route<dynamic> generateRoute(RouteSettings settings) {
@@ -44,6 +52,24 @@ class PageRouter {
         return MaterialPageRoute(builder: (_) => AyarlarPage());
       case RAPORLAR_PAGE_ROUTE:
         return MaterialPageRoute(builder: (_) => RaporlarPage());
+      case YENI_URUN_HIZMET_PAGE_ROUTE:
+        return MaterialPageRoute(builder: (_) => YeniUrunHizmetPage());
+      case ALISLAR_YENI_FATURA_PAGE_ROUTE:
+        return MaterialPageRoute(builder: (_) => YeniFaturaAlislarPage());
+      case ALISLAR_YENI_ODEME_PAGE_ROUTE:
+        return MaterialPageRoute(builder: (_) => YeniOdemeAlislarPage());
+      case ALISLAR_YENI_TEDARIKCI_PAGE_ROUTE:
+        return MaterialPageRoute(builder: (_) => YeniTedarikciAlislarPage());
+      case SATISLAR_YENI_MUSTERI_PAGE_ROUTE:
+        return MaterialPageRoute(builder: (_) => YeniMusteriSatislarPage());
+      case SATISLAR_YENI_FATURA_PAGE_ROUTE:
+        return MaterialPageRoute(builder: (_) => YeniFaturaSatislarPage());
+      case SATISLAR_YENI_TAHSILAT_PAGE_ROUTE:
+        return MaterialPageRoute(builder: (_) => YeniFaturaSatislarPage());
+      case BANKALAR_YENI_HESAP_PAGE_ROUTE:
+        return MaterialPageRoute(builder: (_) => YeniHesapBankalarPage());
+      case BANKALAR_YENI_TRANSFER_PAGE_ROUTE:
+        return MaterialPageRoute(builder: (_) => YeniTransferBankalarPage());
       default:
         return MaterialPageRoute(builder: (_) => YeniTahsilatSatislarPage());
     }
